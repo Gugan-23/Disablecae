ac52830f692bec5e38fc8fe12a709cf81e8c9d61
Gugan <v.gugan16@gmail.com>
2024-08-17 16:41:21 +0530

    hi

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './AppRouter';  // Correctly reference the new file
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
+
